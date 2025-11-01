91be9fcdccae595729cc9bfd394f42e6855dc27f
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-11-01 14:57:33 +0700

    User service: build all entity

---
+++ user-service/src/main/java/com/bank/user_service/domain/entity/PersonalInformation.java
@@ -0,0 +1,51 @@
+package com.bank.user_service.domain.entity;
+
+import com.bank.user_service.infrastructure.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("personal_information")
+public class PersonalInformation extends BaseEntity {
+  @Column("first_name")
+  private String firstName;
+
+  @Column("last_name")
+  private String lastName;
+
+  @Column("date_of_birth")
+  private Long dateOfBirth;
+
+  @Column("gender")
+  private Gender gender;
+
+  @Column("personal_photo")
+  private String personalPhoto;
+
+  @Column("country")
+  private String country;
+
+  @Column("province")
+  private String province;
+
+  @Column("district")
+  private String district;
+
+  @Column("ward")
+  private String ward;
+
+  @Column("street")
+  private String street;
+
+  @Column("home_number")
+  private String homesNumber;
+}
---
+++ user-service/src/main/java/com/bank/user_service/infrastructure/enums/Gender.java
@@ -0,0 +1,3 @@
+package com.bank.user_service.infrastructure.enums;
+
+public enum Gender {}
