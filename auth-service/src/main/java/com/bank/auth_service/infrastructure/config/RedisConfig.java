ed75981f8f135911897f8159ee9c20ccf2aa91d0
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-11-01 10:38:55 +0700

    Auth service: config redis

---
+++ auth-service/src/main/java/com/bank/auth_service/infrastructure/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.bank.auth_service.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+  @Bean(name = "redis-jwt")
+  public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
+      ReactiveRedisConnectionFactory factory) {
+
+    StringRedisSerializer stringSerializer = new StringRedisSerializer();
+
+    RedisSerializationContext.RedisSerializationContextBuilder<String, String> builder =
+        RedisSerializationContext.newSerializationContext(stringSerializer);
+
+    RedisSerializationContext<String, String> context =
+        builder
+            .key(stringSerializer)
+            .value(stringSerializer)
+            .hashKey(stringSerializer)
+            .hashValue(stringSerializer)
+            .build();
+    return new ReactiveRedisTemplate<>(factory, context);
+  }
+}
