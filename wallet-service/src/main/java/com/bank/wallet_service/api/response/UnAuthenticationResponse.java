3cc4febb663c309ecb84c36743b63320e524d377
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-11-10 19:38:06 +0700

    Wallet service: implement security

---
+++ wallet-service/src/main/java/com/bank/wallet_service/api/response/UnAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.bank.wallet_service.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnAuthenticationResponse {
+  private String error;
+  private String message;
+  private Integer statusCode;
+}
